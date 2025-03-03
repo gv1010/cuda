160dd43c590cca95d005977d021a516c353e5c8f
gv1010 <gv281093@gmail.com>
2025-03-03 22:07:37 +0530

    Update matrix_mul.cu
    corrected the misakes, still debugging is needed to small matrix multiplication

--- matrix_mul.cu
+++ matrix_mul.cu
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <cuda_runtime.h>
 
+# define TILE_WIDTH 32
+
 __global__ void navie_mat_mul(float *P_A, float *P_B,float *P_C, int Width){
     int row = blockDim.y*blockIdx.y + threadIdx.y;
     int col = blockDim.x*blockIdx.x + threadIdx.x;
@@ -15,64 +17,83 @@ __global__ void navie_mat_mul(float *P_A, float *P_B,float *P_C, int Width){
 }
 
 __global__ void  mat_mul(float *A, float *B, float *C, int width){
-    __shared__ float As[32][32];
-    __shared__ float Bs[32][32];
+    __shared__ float As[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float Bs[TILE_WIDTH][TILE_WIDTH];
 
     int bx = blockIdx.x;
     int by = blockIdx.y;
     int tx = threadIdx.x;
     int ty = threadIdx.y;
 
-    int row = by * blockDim.y + ty;
-    int col = bx + blockDim.x + tx;
+    int row = by * TILE_WIDTH + ty;
+    int col = bx * TILE_WIDTH + tx;
 
     float element_value = 0;
 
-    As[ty][tx] = A[row*width + tx];
-    Bs[ty][tx] = B[ty*width + col];
+    for (int t=0; t < int (width+TILE_WIDTH - 1)/TILE_WIDTH; t ++){
 
-    __syncthreads();
-
-    for (int k=0; k < blockDim.x; k++){
-        element_value += As[ty][k]* Bs[k][tx];
-    }
+        if ((row < width) && ((t*TILE_WIDTH + tx) < width)){
+            As[ty][tx] = A[row*width + t*TILE_WIDTH + tx];
+        }
+        else{
+            As[ty][tx] = 0.0f;
+        }
     
-    C[row*width + col] = element_value;
+        if ((col < width) && ((t*TILE_WIDTH + ty) < width)){
+            Bs[ty][tx] = B[(t*TILE_WIDTH + ty)*width + col];
+        }
+        else{
+            Bs[ty][tx] = 0.0f;
+        }
+        __syncthreads();
 
+        
+        for (int k=0; k < TILE_WIDTH; k++){
+            element_value += As[ty][k]* Bs[k][tx];
+        }
+        __syncthreads();
+
+    }
+    if ((row < width) && (col < width)){
+        C[row*width + col] = element_value;
+    }
 
 }
 
 
 int main(){
-    int rows = 2048;
-    int cols = 2048;
+    int rows = 1024;
+    int cols = 1024;
     int size = rows * cols; 
-    float *hA, *hB, *hC; // Host arrays
-    float *dA, *dB, *dC; // Device arrays
+    float *hA, *hB, *hC, *hC_naive; // Host arrays
+    float *dA, *dB, *dC, *dC_naive; // Device arrays
 
     // Allocate memory on host
     hA = new float[size];
     hB = new float[size];
     hC = new float[size];
+    hC_naive = new float[size];
 
     // Allocate memory on device
     cudaMalloc((void **)&dA, size * sizeof(float));
     cudaMalloc((void **)&dB, size * sizeof(float));
     cudaMalloc((void **)&dC, size * sizeof(float));
 
-
+    cudaMalloc((void **)&dC_naive, size * sizeof(float));
 
     // Initialize host arrays (example values)
     for (int row = 0; row < rows; row ++){
         for (int col = 0; col < cols; col++){
-            hA[row*cols + col] = row*cols + col;
+            // hA[row*cols + col] = row*cols + col;
+            hA[row*cols + col] = 1;
         }
     }
 
     // Initialize host arrays (example values)
     for (int row = 0; row < rows; row ++){
         for (int col = 0; col < cols; col++){
-            hB[row*cols + col] = row*cols + col;
+            // hB[row*cols + col] = row*cols + col;
+            hB[row*cols + col] = 1;
         }
     }
 
@@ -83,7 +104,7 @@ int main(){
 
     // Kernel launch configuration
     dim3 blockSize(32, 32, 1);
-    dim3 gridSize((cols + blockSize.x - 1) / blockSize.x, (rows + blockSize.y - 1) / blockSize.y, 1);
+    //dim3 gridSize((cols + blockSize.x - 1) / blockSize.x, (rows + blockSize.y - 1) / blockSize.y, 1);
 
     printf("%25s%25s%25s\n", "Exectution", "Bandwidth (GB/s)", "GPU Time (ms)");
     printf("\n");
@@ -98,7 +119,7 @@ int main(){
 
     for (int i=0; i < num_runs; i++){
         cudaEventRecord(start1);
-        mat_mul<<<gridSize, blockSize>>>(dA, dB, dC, rows);
+        mat_mul<<<1, blockSize>>>(dA, dB, dC, rows);
         cudaEventRecord(stop1);
         cudaEventSynchronize(stop1);
         float milliseconds1 = 0;
@@ -112,7 +133,7 @@ int main(){
     printf("%20.4f%25.4f\n", 2 * size * sizeof(float)* 1e-6 / average_milliseconds1, average_milliseconds1 );
 
 
-
+    
     printf("%25s","Navie Matrix Mul");
 
     cudaEvent_t start2, stop2;
@@ -123,7 +144,7 @@ int main(){
 
     for (int i=0; i < num_runs1; i++){
         cudaEventRecord(start2);
-        navie_mat_mul<<<gridSize, blockSize>>>(dA, dB, dC, rows);
+        navie_mat_mul<<<1, blockSize>>>(dA, dB, dC_naive, rows);
         cudaEventRecord(stop2);
         cudaEventSynchronize(stop2);
         float milliseconds2 = 0;
@@ -133,15 +154,44 @@ int main(){
     float average_milliseconds2 = total_milliseconds2 / num_runs1;
     cudaEventDestroy(start2);
     cudaEventDestroy(stop2);
-    cudaMemcpy(hC, dC, size * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(hC_naive, dC_naive, size * sizeof(float), cudaMemcpyDeviceToHost);
+
     printf("%20.4f%25.4f\n", 2 * size * sizeof(float)* 1e-6 / average_milliseconds2, average_milliseconds2 );
+
+    
+    printf("hA: \n ");
+    for (int row = 0; row < 5; row ++){
+        for (int col = 0; col < 5; col++){
+            printf("%0.0f ", hA[row*cols + col]);
+        }
+        printf("\n");
+    }
+    
+    printf("hB: \n");
+    for (int row = 0; row < 5; row ++){
+        for (int col = 0; col < 5; col++){
+            printf("%0.0f ", hB[row*cols + col]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
+    printf("hC: \n");
+    for (int row = 0; row < 5; row ++){
+        for (int col = 0; col < 5; col++){
+            printf("%0.f ", hC[row*cols + col]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
+    printf("hC_naive: \n");
+    for (int row = 0; row < 5; row ++){
+        for (int col = 0; col < 5; col++){
+            printf("%0.f ", hC_naive[row*cols + col]/num_runs1);
+        }
+        printf("\n");
+    }
     return 0;
 
 }
-
-
-//  Exectution         Bandwidth (GB/s)            GPU Time (ms)
-
-// Matrix Mul             46.1203                   0.7275
-// Navie Matrix Mul        0.4569                  73.4422
-
